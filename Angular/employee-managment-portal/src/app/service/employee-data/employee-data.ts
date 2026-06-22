bf283cb29537e0b5bd0a5dfd2383beb332d2056a
Kenil-2005 <pansarakenil0891@gmail.com>
2026-06-22 23:51:25 +0530

    commit

--- Angular/employee-managment-portal/src/app/service/employee-data/employee-data.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-employee-data',
-  imports: [],
-  templateUrl: './employee-data.html',
-  styleUrl: './employee-data.css',
-})
-export class EmployeeData {}
